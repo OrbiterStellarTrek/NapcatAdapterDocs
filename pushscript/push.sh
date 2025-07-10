aaac21d0df35ba02549bc21d00b6e27850ea8018
皮梦 <pimeng@vip.qq.com>
2025-07-10 23:43:52 +0800

    fix: 文档

---
+++ pushscript/push.sh
@@ -0,0 +1,19 @@
+cd ..
+mkdir temp
+GITHUB_URL=https://oauth:${PushKey}@github.com/OrbiterStellarTrek/ncadocs.github.io
+git clone ${GITHUB_URL} temp
+
+rm -rf temp/*
+mv .vitepress/dist/* temp
+
+cd ./temp
+
+git config --global init.defaultBranch main
+git remote add origin ${GITHUB_URL}
+git branch -M main
+
+git config --global user.name "Page Build"
+git config --global user.email "pimeng7143@gmail.com"
+git add *
+git commit -m "docs: 构建更新"
+git push -f
