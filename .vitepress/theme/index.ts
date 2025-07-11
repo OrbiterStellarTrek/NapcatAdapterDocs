08e6147fe407ba86bc3b394d47e2ecbb2e8605cb
Xt-0 <sczr0710@163.com>
2025-07-11 17:25:28 +0800

    feat: 添加css文件的导入

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,14 +3,10 @@ import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import Confetti from "./Confetti.vue";
+import './custom.css'
 
 export default {
   extends: DefaultTheme,
-  Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
-  },
   enhanceApp({ app, router, siteData }) {
     app.component("Confetti", Confetti);
   }
