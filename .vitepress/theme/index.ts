c1e3438cc391e1abbd3dcc88ed5fd41cadda7afe
Xt-0 <sczr0710@163.com>
2025-07-11 17:12:02 +0800

    feat: 添加css文件的导入

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,6 +3,7 @@ import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import Confetti from "./Confetti.vue";
+import './custom.css'
 
 export default {
   extends: DefaultTheme,
