e1496ffa459cfea13925e63e048f213b64d917ef
皮梦 <i@pmya.xyz>
2025-09-04 20:18:49 +0800

    fix: 修改依赖文件，修复纸屑构建时出现的客户端问题

--- .vitepress/theme/Confetti.vue
+++ .vitepress/theme/Confetti.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
+import { onMounted } from 'vue'
 import confetti from "canvas-confetti";
 
-/* 纸屑 */
-confetti({
-  particleCount: 300,
-  spread: 200,
-  origin: { y: 0.4 },
-});
+onMounted(() => {
+  /* 纸屑 */
+  confetti({
+    particleCount: 300,
+    spread: 200,
+    origin: { y: 0.4 },
+  });
+})
 </script>
